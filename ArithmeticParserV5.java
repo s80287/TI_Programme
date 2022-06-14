029d7781c79b9899985195391f8bf59f45fe3aff
Tien Nguyen <72783355+s80287@users.noreply.github.com>
2022-06-14 12:01:03 +0200

    Update ArithmeticParserV5.java
    Edit function Key()

--- ArithmeticParserV5.java
+++ ArithmeticParserV5.java
@@ -5,67 +5,97 @@ public class ArithmeticParserV5 {
 	// attributes
 	String input;
 	int pos;
-
+	
+	HashMap<String, Integer> variableHashTable = new HashMap<String, Integer>();
+	
 	// grammar
 	// S -> let D in E | E
 	// D -> Key = Value {, Key = Value}
 	
 	
 	// methods
-	int parse(String input0, HashMap<String, Integer> variableHashTable) {
+	boolean isDigit(char c) {
+		return '0' <= c && c <= '9';
+	}
+	
+	
+	boolean isLetter(char c) {
+		return 'A' <= Character.toUpperCase(c) && Character.toUpperCase(c) <= 'Z';
+	}
+	
+	
+	// variable name can only contain letter, number and underscore ('_')	
+	boolean isCharacterValidForVariableName(char c) {
+		return isLetter(c) || isDigit(c) || c == '_';
+	}
+	
+	
+	int parse(String input0) {
+		// clear white space
 		// \\s: white space
 		this.input = input0.replaceAll("\\s", "") + "#";
 		this.pos = 0;
 		int res = 0;
-		
-		//res = E(variableHashTable);	match('#');
 		
 		// letx1=1,x2=2inx1+x2
 		// ==: reference equal, equals(): value equal
 		// S -> let D in E | E
 		if (next() == 'l') {
 			match('l'); match('e'); match('t');
-			D(variableHashTable);
+			D();
 			match('i'); match('n');
-			res = E(variableHashTable);
+			res = E();
 			match('#');
 		}
 		// 43*125
 		else {
-			res = E(variableHashTable);	match('#');
+			res = E();	match('#');
 		}
 		
 		return res;
 		
 	}
 	
-
-	void D(HashMap<String, Integer> variableHashTable) {
+	
+	void D() {
 		String key = "";
 		int value;
-		key = Key(variableHashTable); match('='); value = Value(variableHashTable);
+		key = Key(); match('='); value = Value();
 		variableHashTable.put(key, value);
 		
 		while (next() == ',') {
 			match(',');
-			key = Key(variableHashTable); match('='); value = Value(variableHashTable);
+			key = Key(); match('='); value = Value();
 			variableHashTable.put(key, value);
 		}
-	}
+	} 
 	
 	
-	String Key(HashMap<String, Integer> variableHashTable) {
+	String Key() {
 		String res = "";
 		
-		while (next() != '=') {
-			res += Character.toString(next());
-			pos++;
+		// variable can only start with a letter
+		if (isLetter(next())) {			
+			while (next() != '=') {
+				// variable name can only contain letter, number and underscore ('_')
+				if (isCharacterValidForVariableName(next())) {
+					res += Character.toString(next());
+					pos++;
+				}
+				else {
+					throw new IllegalArgumentException(next() + " unexpected, variable name can only contain letter, digit and underscore!");
+				}
+			}
+		}	
+		else {
+			throw new IllegalArgumentException(next() + " unexpected, variable name can only start with a letter!");
 		}
 		
 		return res;
 	}
 	
-	int Value(HashMap<String, Integer> variableHashTable) {
+	
+	int Value() {
 		int res = 0;
 		int sign = 1;
 		
@@ -76,25 +106,25 @@ public class ArithmeticParserV5 {
 		}
 		
 		do {
-			res = 10 * res + Num2();
+			res = 10 * res + Num();
 		} while (next() != 'i' && next() != ',');
+		// i in 'in'
 		
 		return res * sign;
 	}
 	
 	
-	
-	int E(HashMap<String, Integer> variableHashTable) {
-		int res = T(variableHashTable);
+	int E() {
+		int res = T();
 		
 		while (next() == '+' || next() == '-') {
 			if (next() == '+') {
 				match('+');
-				res += T(variableHashTable);
+				res += T();
 			}
 			else {
 				match('-');
-				res -= T(variableHashTable);
+				res -= T();
 			}
 		}
 		
@@ -102,17 +132,17 @@ public class ArithmeticParserV5 {
 	}
 	
 	
-	int T(HashMap<String, Integer> variableHashTable) {
-		int res = F(variableHashTable);
+	int T() {
+		int res = F();
 		
 		while (next() == '*' || next() == '/') {
 			if (next() == '*') {
 				match('*');
-				res *= F(variableHashTable);
+				res *= F();
 			}
 			else {
 				match('/');
-				res /= F(variableHashTable);
+				res /= F();
 			}
 		}
 		
@@ -120,25 +150,20 @@ public class ArithmeticParserV5 {
 	}
 	
 	
-	int F(HashMap<String, Integer> variableHashTable) {
+	int F() {
 		int res;
 		
 		if (next() == '(') {
-			match('(');	res = E(variableHashTable);	match(')');
+			match('(');	res = E();	match(')');
 		}
 		else
-			res = Num1(variableHashTable);
+			res = NumOrVar();
 		
 		return res;
 	}
 	
 	
-	boolean isDigit(char c) {
-		return '0' <= c && c <= '9';
-	}
-	
-	
-	int Num1(HashMap<String, Integer> variableHashTable) {
+	int NumOrVar() {
 
 		int res = 0;
 		int sign = 1;
@@ -153,7 +178,7 @@ public class ArithmeticParserV5 {
 		// digit (> 9)
 		if (isDigit(next())) {
 			do {
-				res = 10 * res + Num2();
+				res = 10 * res + Num();
 			} while (next() != '+' && next() != '-' && next() != '*' 
 					&& next() != '/' && next() != ')' && 
 					next() != '#');
@@ -162,6 +187,7 @@ public class ArithmeticParserV5 {
 		}
 		// variable
 		else {
+			// parse key
 			while (next() != '+' && next() != '-' && next() != '*' 
 					&& next() != '/' && next() != ')' && 
 					next() != '#') {
@@ -172,13 +198,9 @@ public class ArithmeticParserV5 {
 		}
 		
 	}
-
-	int Var(HashMap<String, Integer> variableHashTable, String variable) {
-		return variableHashTable.get(variable);
-	}
 	
 	
-	int Num2() {
+	int Num() {
 		switch(next()) {
 			case '0': match('0'); return 0;
 			case '1': match('1'); return 1;
@@ -190,9 +212,6 @@ public class ArithmeticParserV5 {
 			case '7': match('7'); return 7;
 			case '8': match('8'); return 8;
 			case '9': match('9'); return 9;
-			//case 'x': match('x'); return x;
-			//case 'y': match('y'); return y;
-			//case 'z': match('z'); return z;
 			default: throw new IllegalArgumentException(next() + " unexpected");
 		}
 		
@@ -213,44 +232,22 @@ public class ArithmeticParserV5 {
 	
 	public static void main(String[] args) {
 		ArithmeticParserV5 p = new ArithmeticParserV5();
-		
-		HashMap<String, Integer> variableHashMap = new HashMap<String, Integer>();
-		
-		//variables.put("x", 10);
-		//variables.put("y", 17);
-		//variables.put("z", 12);
-		
-		// test
-		// 1234 true
-		//System.out.println(p.parse("1234"));
-		// 42 true
-		//System.out.println(p.parse("x1"));
-		// -1 true
-		//System.out.println(p.parse("x2"));
-		// a unexpected true
-		//System.out.println(p.parse("a"));
-		// 2 true
-		//System.out.println(p.parse("38/(17+2)"));
+			
 		// 696 true
-		//System.out.println(p.parse("y*(x1+x2)-1"));
+		System.out.println(p.parse("let x1 = 42, x2 = -1, y = 17 in y*(x1+x2) - 1"));
 		// 7212 true
-		//System.out.println(p.parse("12*(34+567)"));
-		// 696 with white space
-		//System.out.println(p.parse("y*(x1+x2) - 1"));
-	
-		//System.out.println(p.parse("let x1=1,x2=2 in x1+x2"));
+		System.out.println(p.parse("12*(34+567)"));
 		
-		// 12 true
-		//System.out.println(p.parse("z", variables));
+		// test variable name
+		// false !
+		//System.out.println(p.parse("let x!2=10 in x!2"));
+		// false 1
+		//System.out.println(p.parse("let 1+1=6 in 1+1+3"));		
 		
-		// 25 true
-		//System.out.println(p.parse("let x1=10, y1=15 in x1+y1", variables));
+		// key not in hash table
+		// false, NullPointerException
+		//System.out.println(p.parse("let x=1, y=2 in z+2"));
 		
-		// 696 true
-		System.out.println(p.parse("let x1 = 42, x2 = -1, y = 17 in y*(x1+x2) - 1", variableHashMap));
-		
-		// 7212 true
-		System.out.println(p.parse("12*(34+567)", variableHashMap));
 	}
 
 }
